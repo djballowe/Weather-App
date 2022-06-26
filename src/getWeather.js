7bfae916f55b43aa92f8253978633d0e7ee0cca4
djballowe <djballowe@gmail.com>
2022-06-26 13:02:24 -0700

    got the class to work

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,5 +1,6 @@
+/* eslint-disable import/no-mutable-exports */
+/* eslint-disable prefer-const */
 // eslint-disable-next-line import/prefer-default-export
-export const info = [];
 
 class WeatherCreator {
   constructor(description, city, temp, feels, wind, humidity) {
@@ -12,22 +13,40 @@ class WeatherCreator {
   }
 }
 
+const description = document.getElementById('description');
+const city = document.getElementById('city');
+const temp = document.getElementById('temp');
+const feels = document.getElementById('feels');
+const wind = document.getElementById('wind');
+const humidity = document.getElementById('humidity');
+
+function weatherDom(weather) {
+  description.textContent = weather.description;
+  city.textContent = weather.city;
+  temp.textContent = weather.temp;
+  feels.textContent = weather.feels;
+  wind.textContent = weather.wind;
+  humidity.textContent = weather.humidity;
+}
+
 async function getWeather() {
   const response = await fetch(
     'http://api.openweathermap.org/data/2.5/weather?q=san%20francisco&APPID=6d73026f9846ee18797b582be8d69941',
   );
   const weatherData = await response.json();
-  info.push(
-    new WeatherCreator(
-      weatherData.weather[0].description,
-      weatherData.name.toUpperCase(),
-      Math.round((weatherData.main.temp - 273.15) * (9 / 5) + 32, 10),
-      Math.round((weatherData.main.temp - 273.15) * (9 / 5) + 32, 10),
-      `Wind: ${weatherData.wind.speed} MPH`,
-      `Humidity: ${weatherData.main.humidity}%`,
-    ),
+
+  const weather = new WeatherCreator(
+    weatherData.weather[0].description,
+    weatherData.name.toUpperCase(),
+    Math.round((weatherData.main.temp - 273.15) * (9 / 5) + 32, 10),
+    Math.round((weatherData.main.temp - 273.15) * (9 / 5) + 32, 10),
+    `Wind: ${weatherData.wind.speed} MPH`,
+    `Humidity: ${weatherData.main.humidity}%`,
   );
+  weatherDom(weather);
 }
 
-export { getWeather };
-console.log(info);
+getWeather();
+weatherDom();
+
+export { getWeather, weatherDom };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import { converter } from './converter';
-import { weatherDom } from './Dom';
-import { getWeather } from './getWeather';
+import { weatherDom, getWeather } from './getWeather';
 
 const button = document.getElementById('converter');
 
 button.addEventListener('click', () => {
   converter();
 });
-
-getWeather();
-weatherDom();
