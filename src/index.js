b950f7d9881ebe01f865a6a59e1d051f7d885626
djballowe <djballowe@gmail.com>
2022-06-26 08:23:35 -0700

    dom updates with all info needed from API

--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ const wind = document.getElementById('wind');
 const humidity = document.getElementById('humidity');
 
 async function getWeather() {
-  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=san%20francisco&APPID=6d73026f9846ee18797b582be8d69941');
+  const response = await fetch(
+    'http://api.openweathermap.org/data/2.5/weather?q=san%20francisco&APPID=6d73026f9846ee18797b582be8d69941',
+  );
   const weatherData = await response.json();
   console.log(weatherData);
 
   description.textContent = weatherData.weather[0].description.toUpperCase();
   city.textContent = weatherData.name.toUpperCase();
+  temp.textContent = parseInt((weatherData.main.temp - 273.15) * (9 / 5) + 32, 10);
+  feels.textContent = `Feels Like: ${parseInt((weatherData.main.feels_like - 273.15) * (9 / 5) + 32, 10)}`;
+  wind.textContent = `Wind: ${weatherData.wind.speed} MPH`;
+  humidity.textContent = `Humidity: ${weatherData.main.humidity}%`;
 }
 
 getWeather();
